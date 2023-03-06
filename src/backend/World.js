f406bb8dbfbf00483c9a96be460413336f860269
Robin <robintyler87@gmail.com>
2023-03-06 13:22:28 +0000

    Add World fetch API call and block1 tests 1st half on Green

--- src/backend/World.js
+++ src/backend/World.js
@@ -1,17 +1,24 @@
 class World {
   constructor() {
     this.continents = [];
-    this.#fetchContinents();
   }
 
-  #fetchContinents() {
-    const response = { testJsonInt: 1, testJsonStr: "string" };
-    this.continents.push(response);
+  async fetchContinents() {
+    try {
+      const response = await fetch("https://api.teleport.org/api/continents/");
+      const data = await response.json();
+      const data2 = await data._links["continent:item"]
+      this.continents.push(data2)
+      console.log(data2);
+    } catch (err) {
+      console.log(err)
+      return null
+    }
   }
 
-  #formatContinentsList() {}
+  // #formatContinentsList() {}
 
-  #addToContinentsList() {}
+  // #addToContinentsList() {}
 }
 
 module.exports = World;
--- src/spec/unit/World.test.js
+++ src/spec/unit/World.test.js
@@ -1,17 +1,49 @@
 import World from "../../backend/World";
+require("jest-fetch-mock").enableMocks();
 
 describe("Block 1: World class", () => {
-  // beforeEach(() => {});
+  let testWorldInstance;
+  let mockApiContinentBasic;
 
-  test("test1 - fetchAPI response is added to array", () => {
-    const worldTestInstance = new World();
-    // const mockAPIjson = {city: "London", score: 10, details: "big place"}
-    expect(worldTestInstance.continents.length).toBe(1);
+  beforeEach(() => {
+    testWorldInstance = new World();
+    mockApiContinentBasic = {
+      _links: { "continent:item": [{}], curies: [{}], self: {} },
+      count: 0,
+    };
+    fetch.resetMocks();
   });
 
-  //test2 - fetchAPI response filtered by params1
-  //test3 - fetchAPI response filtered by params2
-  //test4 - fetchAPI response filtered by params3
-  //test5 - fetchAPI response added to array as single object
-  //test6 - fetchAPI response added to array as multiple objects
+  it("test1 - fetchAPI response is added to array", async () => {
+    fetch.mockResponseOnce(JSON.stringify(mockApiContinentBasic));
+    expect(testWorldInstance.continents.length).toBe(0);
+    await testWorldInstance.fetchContinents();
+    expect(testWorldInstance.continents.length).toBe(1);
+  });
+
+  it("test2 - returns error message if api call fails", async () => {
+    fetch.mockReject(() => Promise.reject("API not working"));
+    const testReturn = await testWorldInstance.fetchContinents();
+    expect(testReturn).toEqual(null);
+    expect(fetch).toHaveBeenCalledWith(
+      "https://api.teleport.org/api/continents/"
+    );
+  });
+
+  it("test3 - filter api response to clear out un-needed data 1, '_links' header with count", async () => {
+    fetch.mockResponseOnce(JSON.stringify(mockApiContinentBasic));
+    await testWorldInstance.fetchContinents();
+    expect(testWorldInstance.continents[0]).not.toHaveProperty("_links");
+  });
+
+  it("test4 - filter api response to clear out un-needed data 2;'curies' & 'self'", async () => {
+    fetch.mockResponseOnce(JSON.stringify(mockApiContinentBasic));
+    await testWorldInstance.fetchContinents();
+    expect(testWorldInstance.continents[0]).not.toHaveProperty("curies");
+    expect(testWorldInstance.continents[0]).not.toHaveProperty("self");
+  });
 });
+
+//test5 - fetchAPI response filtered by params3
+//test6 - fetchAPI response added to array as single object(expect object at index 0 to have a property)
+//test7 - fetchAPI response added to array as multiple objects(expect length > than 1, and obj at index 1 or 2 to have a property(x))
